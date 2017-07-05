ebc6bc857396369b1a0e74a287cc79b369462b5c
oingbong <oingbong@gmail.com>
2017-07-05 11:47:47 +0900

    실제 url 비교 후 세팅한 번호가 comment 번호글인 경우 넘어가도록 설정하였습니다.

--- insert.js
+++ insert.js
@@ -4,61 +4,74 @@ var cheerio     = require('cheerio');
 var http        = require('http');
 var mongoose    = require('mongoose');
 
-var setNumber; // 하나씩 증가하기 위해 글로벌 선언
+var setPageNumber; // 하나씩 증가하기 위해 글로벌 선언
 var dataCheck = []; // 중복 데이터 확인하기 위한 것 , 추후 DB의 값을 가져와서 비교
 
-requestServer(1, 0); // 1: pageNumber
+requestServer(1, 0); // 1: tabNumber
 
-function requestServer(pageNumber, earlyAnswerDate) {
-    console.log('pageNumber : ' + pageNumber);
-    if (setNumber == undefined)
-        //setNumber = 37000000;
-        setNumber = 37000078;
-    	//setNumber = 37004000; 
-        //setNumber = 1335851;
-        //setNumber = 927358;
-        // 37000000 ~ 37003999 까지 진행
-    console.log('PageUrl : ' + setNumber);
+function requestServer(tabNumber, earlyAnswerDate) {
+    // 시작할 게시물 번호 세팅
+    if (setPageNumber == undefined)
+        setPageNumber = 37000000;
 
     var options = {
-        url: 'https://stackoverflow.com/questions/' + setNumber + '?page=' + pageNumber + '&answertab=votes',
+        url: 'https://stackoverflow.com/questions/' + setPageNumber + '?page=' + tabNumber + '&answertab=votes',
         headers : {
             // 'User-Agent' : 'request'
         }
     };
+    console.log('------------------------------');
+    console.log('tabNumber : ' + tabNumber + ' / pageNumber : ' + setPageNumber);
 
     request(options, callback);
 
     function callback(error, response, body){
-        // 페이지 번호가 1 이면 정상 실행, 1 보다 크면 바로 데이터 넘겨줍니다. (이유 : comment만 구하면 되기 때문입니다.)
-        if(pageNumber == 1){
-            $ = cheerio.load(body);
-            let title = $('#question-header > h1 > a');
-            // 페이지를 찾을 수 없는 기준을 html()으로 잡을 수 있습니다.
-            // 이 기준에 적합하면 mongoDB에 데이터 저장해 줍니다.   
-            if (title.html() != null){ 
-                // 해당 데이터가 없다면 -1 , 있다면 인덱스번호 리턴
-                // 나중에 배열 대신에 mongoDB의 제목과 비교 (만약 하게 된다면 일단 mongoDB 에 저장된 타이틀을 배열로 다 가져와서 해야하나?)
-                if(dataCheck.indexOf(title.text()) === -1){
-                    dataCheck.push(title.text()); // 제목기준 데이터 추가
-                    convertData(body, pageNumber, earlyAnswerDate); // data 넘기기
+        /*
+            response.request.href = 현재 페이지 url
+            response.request.href.split('/') = [ 'https:','','stackoverflow.com','questions','18102859','visual-studio-could-not-copy-during-build','37000078#37000078' ]
+            [4] : 실제 게시물의 번호 , [5] : 게시물 제목 , [6] : 내가 요청한 게시물 번호(만약 4번과 일치하지 않는다면 comment 로 작성된 것)
+        */
+
+        let reallyContentNumber = response.request.href.split('/')[4];
+        // 실제 게시물 번호와 세팅한 게시물 번호가 일치하면 정상 실행 , 일치하지 않는다면 코멘트로 간주하고 다음 게시물로 넘어갑니다.
+        if(reallyContentNumber == setPageNumber){
+
+            // 페이지 번호가 1 이면 정상 실행, 1 보다 크면 바로 데이터 넘겨줍니다. (이유 : comment만 구하면 되기 때문입니다.)
+            if(tabNumber == 1){
+                $ = cheerio.load(body);
+                let title = $('#question-header > h1 > a');
+                // 페이지를 찾을 수 없는 기준을 html()으로 잡을 수 있습니다.
+                // 이 기준에 적합하면 mongoDB에 데이터 저장해 줍니다.   
+                if (title.html() != null){ 
+                    // 해당 데이터가 없다면 -1 , 있다면 인덱스번호 리턴
+                    // 나중에 배열 대신에 mongoDB의 제목과 비교 (만약 하게 된다면 일단 mongoDB 에 저장된 타이틀을 배열로 다 가져와서 해야하나?)
+                    if(dataCheck.indexOf(title.text()) === -1){
+                        console.log('신규 데이터');
+                        dataCheck.push(title.text()); // 제목기준 데이터 추가
+                        convertData(body, tabNumber, earlyAnswerDate); // data 넘기기
+                    }else{
+                        console.log('중복 데이터');
+                        setPageNumber++;
+                        requestServer(1, 0);
+                    }
                 }else{
-                    console.log('중복 데이터');
-                    setNumber++;
+                    console.log('게시물 삭제된 URL');
+                    setPageNumber++;
                     requestServer(1, 0);
                 }
-            }else{
-                console.log('게시물 삭제된 URL');
-                setNumber++;
-                requestServer(1, 0);
+            }else if(tabNumber > 1){
+                convertData(body, tabNumber, earlyAnswerDate); // data 넘기기
             }
-        }else if(pageNumber > 1){
-            convertData(body, pageNumber, earlyAnswerDate); // data 넘기기
+        }else{
+            console.log('코멘트 게시글이므로 다음 번호로 넘어갑니다.');
+            setPageNumber++;
+            requestServer(1, 0);
         }
     }
+
 }
 
-function convertData(data, pageNumber, earlyAnswerDate){
+function convertData(data, tabNumber, earlyAnswerDate){
     var commentArr = []; // commentId 를 넣는 배열
 
     $ = cheerio.load(data);
@@ -78,12 +91,12 @@ function convertData(data, pageNumber, earlyAnswerDate){
     let questionUserUrl = parseQuestionUserUrl.attr('href'); // href 속성값 가져오기 위해 사용
     let answersCount = parseAnswersCount.text(); // text() , html() 둘다 같음
 
-    console.log('title : ' + title);
-    console.log('viewCount : ' + viewCount); 
-    console.log('questionTime : ' + questionTime);
-    console.log('questionUserName : ' + questionUserName);
-    console.log('questionUserUrl : ' + questionUserUrl);
-    console.log('answersCount : ' + answersCount);
+    // console.log('title : ' + title);
+    // console.log('viewCount : ' + viewCount); 
+    // console.log('questionTime : ' + questionTime);
+    // console.log('questionUserName : ' + questionUserName);
+    // console.log('questionUserUrl : ' + questionUserUrl);
+    // console.log('answersCount : ' + answersCount);
 
     /*
         응답 시간 구하기 위해 응답의 각 id 를 배열로 가져옵니다.
@@ -97,15 +110,15 @@ function convertData(data, pageNumber, earlyAnswerDate){
 
     // comment 가 있다면 데이터 추가하고 없다면 다음페이지 이동( 기준 : answersCount)
     if(answersCount == ''){
-        setNumber++;
+        setPageNumber++;
         requestServer(1, 0);
         mongoConnectAndInsertData(title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount, null);
     }else{
-        comment(data, commentArr, earlyAnswerDate, pageNumber, title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount);
+        comment(data, commentArr, earlyAnswerDate, tabNumber, title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount);
     }
 }
 
-function comment(data, arr, earlyAnswerDate, pageNumber, title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount){
+function comment(data, arr, earlyAnswerDate, tabNumber, title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount){
     var findQuickDate = []; // answer 중에 가장 빠른 시간 찾기 위한 배열
 
     $ = cheerio.load(data);
@@ -117,7 +130,7 @@ function comment(data, arr, earlyAnswerDate, pageNumber, title, viewCount, quest
     // 가장 빠른 시간 구하기
     for(var i=0; i < arr.length; i++){
         let answersTime = $('#answers > #'+ arr[i] +' > table > tbody > tr:nth-child(1) > .answercell > table > tbody > tr > td:nth-child(3) > div > .user-action-time > span');
-        console.log('answersTime : ' + answersTime.attr('title'));
+        // console.log('answersTime : ' + answersTime.attr('title'));
         if(answersTime.attr('title') != undefined){ // 날짜가 존재하는 데이터만 배열에 Date로 변환하여 추가하기
             findQuickDate.push(Date.parse(answersTime.attr('title')));
         }
@@ -133,19 +146,15 @@ function comment(data, arr, earlyAnswerDate, pageNumber, title, viewCount, quest
     }
 
     /*
-        null == 다음페이지가 없는 것으로 판단하고 setNumber +1 합니다.
+        null == 다음페이지가 없는 것으로 판단하고 setPageNumber +1 합니다.
         null != 현재페이지에 +1 을 하여 다시 호출합니다.
     */
-    console.log('hasNextButton1 : ' + hasNextButton.html());
-    /*
-        
-    */
     if(hasNextButton.html() != null){
-        requestServer(pageNumber + 1, earliestDate);
+        requestServer(tabNumber + 1, earliestDate);
     }else{
-        setNumber++;
-        requestServer(1, 0);
         mongoConnectAndInsertData(title, viewCount, questionTime, questionUserName, questionUserUrl, answersCount, earliestDate);
+        setPageNumber++;
+        requestServer(1, 0);
     }
 }
 
@@ -169,6 +178,7 @@ function mongoConnectAndInsertData(title, viewCount, questionTime, questionUserN
 	});
 	modelData.save(function(err){
 		if(err) console.log(err);
+        else console.log('DB save title : ' + title);
 	});
 }
 
